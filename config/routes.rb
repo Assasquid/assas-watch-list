f2e6a1cc3287fc637a8814b55d2a80c7913a89b1
Jerome Million Rousseau <jerome@nebula-tales.com>
2023-02-26 18:33:10 +0000

    Improved the Lists, added Bookmark creation and display.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'lists#index'
+
+  # get 'lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new', as: :new_list
+  # post 'lists', to: 'lists#create'
+  # get 'lists/:id', to: 'lists#show', as: :list
+
+  resources :lists, except: [:edit, :update, :destroy] do
+    resources :bookmarks, only: [:new, :create]
+  end
 end
